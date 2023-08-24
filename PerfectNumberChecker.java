8d0c134428dfee960936c43621d88b295478478b
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2023-08-24 16:17:46 +0300

    Add files via upload

---
+++ PerfectNumberChecker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PerfectNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+
+        if (isPerfectNumber(number)) {
+            System.out.println(number + " Mükemmel sayıdır.");
+        } else {
+            System.out.println(number + " Mükemmel sayı değildir.");
+        }
+    }
+
+    public static boolean isPerfectNumber(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        int sum = 1; // 1 is always a divisor
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i != num / i) {
+                    sum += num / i;
+                }
+            }
+        }
+
+        return sum == num;
+    }
+}
